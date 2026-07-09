ddc3a435a32ef5b684c85d6f235693336b3b91e7
G Vishhy <woowoopdi@gmail.com>
2026-07-09 19:53:36 +0530

    0733-FLOOD-FILL Stats: Time: 0 ms (100%), Space: 18 MB (90.85%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,27 @@
 class Solution {
 private:
-    void backtrack(vector<vector<int>>& image, int i, int j, int n, int m, int colour, int prev, vector<vector<bool>>& visited){
-        if(i < 0 || i >= n || j < 0 || j >= m || image[i][j] != prev || visited[i][j] == true){
+    void DFS(vector<vector<int>>& image, int i, int j, int n, int m, int colour, int prev){
+        if(i < 0 || i >= n || j < 0 || j >= m || image[i][j] != prev){
             return;
         }
 
         image[i][j] = colour;
-        visited[i][j] = true;
 
-        backtrack(image, i+1, j, n, m, colour, prev, visited);
-        backtrack(image, i-1, j, n, m, colour, prev, visited);
-        backtrack(image, i, j+1, n, m, colour, prev, visited);
-        backtrack(image, i, j-1, n, m, colour, prev, visited);
+        DFS(image, i+1, j, n, m, colour, prev);
+        DFS(image, i-1, j, n, m, colour, prev);
+        DFS(image, i, j+1, n, m, colour, prev);
+        DFS(image, i, j-1, n, m, colour, prev);
         return;
     }
 
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size(), m = image[0].size();
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
-        backtrack(image, sr, sc, n, m, color, image[sr][sc], visited);
+        int st = image[sr][sc];
+        
+        if(st != color){
+            DFS(image, sr, sc, n, m, color, image[sr][sc]);
+        }
 
         return image;
     }
