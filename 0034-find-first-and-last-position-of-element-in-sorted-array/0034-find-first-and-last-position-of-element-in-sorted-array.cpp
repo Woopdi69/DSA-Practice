d5a7f03ccffe268b750baae2c25f248456e6b44b
G Vishhy <woowoopdi@gmail.com>
2026-07-15 21:40:49 +0530

    0034-FIND-FIRST-AND-LAST-POSITION-OF-ELEMENT-IN-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 17.4 MB (89.89%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -5,21 +5,15 @@ public:
             return {-1, -1};
         }
         int left = 0, right = nums.size()-1;
+        int l = -1, r = -1;
         vector<int> ans(2, -1);
+
         while(left <= right){
             int mid = left+(right-left)/2;
 
             if(nums[mid] == target){
-                int l = mid, r = mid;
-                while(l-1 >= 0 && nums[l-1] == nums[mid]){
-                    l--;
-                }
-                while(r+1 < nums.size() && nums[r+1] == nums[mid]){
-                    r++;
-                }
-                ans[0] = l;
-                ans[1] = r;
-                return ans;
+                l = mid;
+                right = mid-1;
             }else if(nums[mid] < target){
                 left = mid+1;
             }else{
@@ -27,6 +21,23 @@ public:
             }
         }
 
+        left = 0;
+        right = nums.size()-1;
+        while(left <= right){
+            int mid = left+(right-left)/2;
+
+            if(nums[mid] == target){
+                r = mid;
+                left = mid+1;
+            }else if(nums[mid] > target){
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+
+        ans[0] = l;
+        ans[1] = r;
         return ans;
     }
 };
