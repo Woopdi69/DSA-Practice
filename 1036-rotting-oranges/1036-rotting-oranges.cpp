1bf494af306e37ed1a1cdd76b18806623e02943b
G Vishhy <woowoopdi@gmail.com>
2026-07-18 12:12:51 +0530

    1036-ROTTING-ORANGES Stats: Time: 10 ms (5.05%), Space: 18.1 MB (5.22%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,45 +1,49 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size(), ans = 0, fresh = 0;
-        vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int n = grid.size(), m = grid[0].size(), time = 0, normal = 0;
         queue<pair<int, int>> q;
+        vector<vector<int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(grid[i][j] == 2){
+                if(grid[i][j] == 1){
+                    normal++;
+                }else if(grid[i][j] == 2){
                     q.push({i, j});
-                }else if(grid[i][j] == 1){
-                    fresh++;
                 }
             }
         }
 
-        if(fresh == 0) return 0;
+        if(normal <= 0) return 0;
 
         while(!q.empty()){
-            int s = q.size();
+            int size = q.size();
             bool found = false;
-            for(int i = 0; i < s; i++){
-                int r = q.front().first;
-                int c = q.front().second;
+
+            for(int k = 0; k < size; k++){
+                auto [i, j] = q.front();
                 q.pop();
 
                 for(auto d : dir){
-                    int nr = r + d[0];
-                    int nc = c + d[1];
+                    int ni = i+d[0];
+                    int nj = j+d[1];
 
-                    if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1){
-                        grid[nr][nc] = 2;
+                    if(ni >= 0 && nj >= 0 && ni < n && nj < m && grid[ni][nj] == 1){
+                        normal--;
+                        grid[ni][nj] = 2;
+                        q.push({ni, nj});
                         found = true;
-                        fresh--;
-                        q.push({nr, nc});
                     }
                 }
             }
-            if(found) ans++;
+            
+            if(found) time++;
         }
 
-        return fresh == 0 ? ans : -1;
+        if(normal > 0){
+            return -1;
+        }
+        return time;
     }
 };
