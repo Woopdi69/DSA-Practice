ae3dbd65e27b73b0dcf1f91205e9031feb917e18
G Vishhy <woowoopdi@gmail.com>
2026-07-24 18:36:44 +0530

    0053-MAXIMUM-SUBARRAY Stats: Time: 0 ms (100%), Space: 71.8 MB (52.99%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = INT_MIN, cur = 0;
-
+        int ans = INT_MIN, cur = 0;
         for(int i = 0; i < nums.size(); i++){
             cur += nums[i];
-
-            sum = max(sum, cur);
+            ans = max(ans, cur);
             if(cur < 0){
                 cur = 0;
             }
         }
 
-        return sum;
+        return ans;
     }
 };
